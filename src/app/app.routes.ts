6708ed34c8ec434efb4414ac1c9294101d47def6
Fa-collab1 <opus.ionam@gmail.com>
2024-04-21 02:41:52 +0200

    initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ConversionComponent } from './pages/conversion/conversion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'conversion', component: ConversionComponent },
+
+];
